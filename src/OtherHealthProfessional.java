6b6275d9c96c1329cb072a108c3366a52305144f
LiuTao <161917514+202200408040@users.noreply.github.com>
2024-11-20 19:15:51 +0800

    Update OtherHealthProfessional.java

--- src/OtherHealthProfessional.java
+++ src/OtherHealthProfessional.java
@@ -1,2 +1,24 @@
-public class AnotherHealthProfessional {
+// OtherHealthProfessional.java
+public class OtherHealthProfessional extends HealthProfessional {
+    private String field;
+
+    // Default constructor
+    public OtherHealthProfessional() {
+        super();
+        this.field = "Specialized Field";
+    }
+
+    // Constructor that initializes all instance variables
+    public OtherHealthProfessional(int id, String name, String basicInfo, String field) {
+        super(id, name, basicInfo);
+        this.field = field;
+    }
+
+    // Override method to print details
+    @Override
+    public void printDetails() {
+        System.out.println("The health professional details are:");
+        super.printDetails();
+        System.out.println("Field: " + field);
+    }
 }
