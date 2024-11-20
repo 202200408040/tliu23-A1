205eb89ca7992f895d81451bb0baeb1607316d0a
LiuTao <161917514+202200408040@users.noreply.github.com>
2024-11-20 19:14:58 +0800

    Update HealthProfessional.java
    There was an error running, re-modify HealthProfessional.java

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
+// HealthProfessional.java
 public class HealthProfessional {
+    private int id;
+    private String name;
+    private String basicInfo;
+
+    // Default constructor
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "";
+        this.basicInfo = "";
+    }
+
+    // Constructor that initializes all instance variables
+    public HealthProfessional(int id, String name, String basicInfo) {
+        this.id = id;
+        this.name = name;
+        this.basicInfo = basicInfo;
+    }
+
+    // Method to print health professional details
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Basic Info: " + basicInfo);
+    }
 }
