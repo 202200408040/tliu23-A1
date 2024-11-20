8cef9eae2f5697550a623a3caada86de9a46913f
LiuTao <161917514+202200408040@users.noreply.github.com>
2024-11-20 18:51:58 +0800

    Update Appointment.java
    There was an error running, re-modify appoint.java

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
+// Appointment.java
 public class Appointment {
+    private String patientName;
+    private String patientMobile;
+    private String timeSlot;
+    private HealthProfessional doctor;
+
+    // Default constructor
+    public Appointment() {
+        this.patientName = "";
+        this.patientMobile = "";
+        this.timeSlot = "";
+        this.doctor = null;
+    }
+
+    // Constructor that initializes all instance variables
+    public Appointment(String patientName, String patientMobile, String timeSlot, HealthProfessional doctor) {
+        this.patientName = patientName;
+        this.patientMobile = patientMobile;
+        this.timeSlot = timeSlot;
+        this.doctor = doctor;
+    }
+
+    // Method to print appointment details
+    public void printDetails() {
+        System.out.println("Patient Name: " + patientName);
+        System.out.println("Patient Mobile: " + patientMobile);
+        System.out.println("Preferred Time: " + timeSlot);
+        System.out.println("Doctor: " + doctor.getClass().getSimpleName());
+        doctor.printDetails();
+    }
+
+    // Getter for patientMobile
+    public String getPatientMobile() {
+        return patientMobile;
+    }
 }
