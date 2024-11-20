03190977d26bf03d0599fcc95324dbe77a41d52f
LiuTao <161917514+202200408040@users.noreply.github.com>
2024-11-20 19:00:41 +0800

    Update GeneralPractitioner.java
    There was an error running, re-modify GeneralPractitioner.java

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+// GeneralPractitioner.java
+public class GeneralPractitioner extends HealthProfessional {
+    private String specialty;
 
-public class GeneralPractitioner {
+    // Default constructor
+    public GeneralPractitioner() {
+        super();
+        this.specialty = "General Medicine";
+    }
+
+    // Constructor that initializes all instance variables
+    public GeneralPractitioner(int id, String name, String basicInfo, String specialty) {
+        super(id, name, basicInfo);
+        this.specialty = specialty;
+    }
+
+    // Override method to print details
+    @Override
+    public void printDetails() {
+        System.out.println("The health professional details are:");
+        super.printDetails();
+        System.out.println("Specialty: " + specialty);
+    }
 }
